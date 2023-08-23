3b20d6425e9e075d8aaf74412bc8ae29655dc662
sai vikas <vishwagnavikassai@gmail.com>
2023-08-23 12:39:59 +0530

    polymorphismsum

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,28 +2,30 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        int sum1 = p.product(2, 3);
+        System.out.println("Product of two integers: " + sum1);
+
+        int sum2 = p.product(2, 3, 4);
+        System.out.println("Product of three integers: " + sum2);
+
+        double sum3 = p.product(2.5, 3.5);
+        System.out.println("Product of two doubles: " + sum3);
+    }
+
     public static class Product {
         public int product(int x, int y) {
-
-            return x+y;
+            return x + y;
         }
 
         public int product(int x, int y, int z) {
-
-            return x*y*z;
+            return x + y + z;
         }
 
         public double product(double x, double y) {
-
-            return x+y;
+            return x + y;
         }
-    }
-
-
-    public static void main(String[] args) {
-        Product p = new Product();
-        p.product(1, 2);
-        p.product(3, 4, 5);
-        p.product(3.45, 35.5666);
     }
 }
